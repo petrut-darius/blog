b9d397dfba499d7aab50f46b01c66b7c37a8fafa
Petrut Darius <eminoviciidarius@gmail.com>
2025-05-12 04:51:23 -0700

    Am bagat flash si erorile pt lungimea textului introdus la articles/new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,9 +7,17 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
-    render plain: params[:article]
+    # aici o fost ceva erroare ca gen nu ma ducea la un url daca l-am scris de mana doar daca i-am dat paste de pe git-u lui
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created succesfully"# un helper care e cam ca un has views/layouts/application
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity# .html.erb
+    end
   end
 end
