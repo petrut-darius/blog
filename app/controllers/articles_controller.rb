7e3afd92085f784c19c98668bd51bd31546075f9
Petrut Darius <eminoviciidarius@gmail.com>
2025-05-12 06:44:19 -0700

    Multe link-uri + edit page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,4 +34,10 @@ class ArticlesController < ApplicationController
       render :new, status: :unprocessable_entity# .html.erb
     end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
 
   root "pages#home"
   get "about", to: "pages#about"
-  resources :articles, only: [ :show, :index, :new, :create, :edit, :update ]
+  resources :articles# acuma ma folosesc de toate restfull_routes
 end
