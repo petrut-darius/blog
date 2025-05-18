2ec691829ca85c1e5d7c83d7f2792e0955f49853
Petrut Darius <eminoviciidarius@gmail.com>
2025-05-18 11:35:06 -0700

    Add users show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def gravatar_for(user, options = { size: 80 })
+    email_address = user.email.downcase
+    hash = Digest::SHA256.hexdigest(email_address)
+    size = options[:size]
+    image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(image_src, alt: user.username, class: "rounded shadow mx-auto d-block")
+  end
 end
